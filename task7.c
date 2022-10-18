f5e740b57c2804894ba20b8701f59c191b60d5a8
Anthony Edward Stark <92053608+sk3pta@users.noreply.github.com>
2022-10-18 23:32:20 +0300

    Update task7.c

--- task7.c
+++ task7.c
@@ -4,6 +4,17 @@
 #define true 1
 #define SIZE 26
 
+bool isArrayNull(char *array, int array_size)
+	{
+	for (int x = 0; x < array_size; ++x) 
+		{
+		if (array[x] != NULL) return false;
+		
+		}
+	return true;
+	}
+
+
 bool isIn(char el, char *array,int array_size) {
 
 	for (int x =0; x < array_size; ++x)
@@ -20,29 +31,47 @@ bool isIn(char el, char *array,int array_size) {
 
 
 int main() {
-FILE *inp_file;
-FILE *out_file;
-inp_file = fopen("input.txt","r");
-out_file = fopen("output.txt","w");
 
-char first_set[SIZE] = {NULL};
-char second_set[SIZE] = {NULL};
 
-fscanf(inp_file,"%s", first_set);
-fscanf(inp_file,"%s", second_set);
+	FILE *inp_file;
+	FILE *out_file;
+	
+	inp_file = fopen("input.txt","r");
+	out_file = fopen("output.txt","w");
 
-fclose(inp_file);
+	char first_set[SIZE] = {NULL};
+	char second_set[SIZE] = {NULL};
+	char output_set[SIZE] = {NULL};
+	
+	fscanf(inp_file,"%s", first_set);
+	fscanf(inp_file,"%s", second_set);
+	fclose(inp_file);
 
-char output_set[SIZE] = {NULL};
-for (int x = 0; x < SIZE; ++x)
+	for (int x = 0; x < SIZE; ++x)
 
-	{
-		char el = first_set[x];
+		{
+			char el = first_set[x];
 		
-		if ( !isIn(el,second_set,SIZE)) {fprintf(out_file,"%c",el);}
+			if ( !isIn(el,second_set,SIZE))
+			{
+			output_set[el % 65] = el;
+			}
 
 
-	}	
+		}	
+
+	
+
+	if(isArrayNull(output_set,SIZE)) fputs("Empty set",out_file);
+	else 
+		{
+
+
+			for (int x =0; x < SIZE; ++x){fputc(output_set[x],out_file);}
+
+
+		}
+
 
 fclose(out_file);
 return 0;
